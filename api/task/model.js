93ce36bf55dd570ea811963b61b8cebdb7bb12dd
Rika Ciminieri <rikaciminieri@gmail.com>
2021-09-26 12:07:57 -0700

    Tasks get/post enpoints completed

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,41 @@
 // build your `Task` model here
+const db = require("../../data/dbConfig");
+
+const getTasks = async () => {
+  const rows = await db("tasks as t")
+    .join("projects as p", "t.project_id", "p.project_id")
+    .select(
+      "t.task_id",
+      "t.task_description",
+      "task_notes",
+      "task_completed",
+      "project_name",
+      "project_description"
+    );
+
+  const updatedRows = rows.map((row) => {
+    if (row.task_completed === 0) {
+      return { ...row, task_completed: false };
+    } else {
+      return { ...row, task_completed: true };
+    }
+  });
+
+  return updatedRows;
+};
+
+const getTaskById = (id) => {
+  return db("tasks").where("task_id", id).first();
+};
+
+const createTask = async (task) => {
+  const [id] = await db("tasks").insert(task);
+  const newTask = await getTaskById(id);
+  if (newTask.task_completed === 0) {
+    return { ...newTask, task_completed: false };
+  } else {
+    return { ...newTask, task_completed: true };
+  }
+};
+
+module.exports = { getTasks, createTask };
--- api/task/router.js
+++ api/task/router.js
@@ -1,4 +1,22 @@
 // build your `/api/tasks` router here
-const router = require("express").Router()
+const router = require("express").Router();
 
-module.exports = router
+const { response } = require("../server");
+const Tasks = require("./model");
+
+router.get("/", (req, res, next) => {
+  Tasks.getTasks()
+    .then((tasks) => {
+      res.status(200).json(tasks);
+    })
+    .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+  Tasks.createTask(req.body)
+    .then((newTask) => {
+      res.status(201).json(newTask);
+    })
+    .catch(next);
+});
+module.exports = router;
