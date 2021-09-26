90e4b977dbb4c8530622b510ed302f2f71073553
Rika Ciminieri <rikaciminieri@gmail.com>
2021-09-25 20:27:21 -0700

    Server running and routers connected

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
 // build your server here and require it from index.js
+const express = require("express");
+
+const server = express();
+const projectsRouter = require("./project/router");
+const resourcesRouter = require("./resource/router");
+const tasksRouter = require("./task/router");
+
+server.use(express.json());
+
+server.use("/api/projects", projectsRouter);
+server.use("/api/resources", resourcesRouter);
+server.use("/api/tasks", tasksRouter);
+
+module.exports = server;
